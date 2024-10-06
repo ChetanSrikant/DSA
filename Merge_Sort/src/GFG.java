48b78b780ad51d540181f0963c83ce6b1dcc5175
ChetanSrikant <chetansmandiga@gmail.com>
2024-10-06 20:25:09 +0530

    merge sort

---
+++ Merge_Sort/src/GFG.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+class GfG {
+
+    public static void main(String[] args)
+    {
+        int[] arr = {9, 5, 1, 4, 3, 7, 6, 8, 2};
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+
+        int mid = (s + e) / 2;
+
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s + l] = mix[l];
+        }
+    }
+}
