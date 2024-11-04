abe2cebf7ad282c49ca6dcc49194410121e6929e
ChetanSrikant <chetansmandiga@gmail.com>
2024-11-05 03:31:15 +0530

    OOP3

---
+++ OOP3/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+//        Box box = new Box(4,5,9);
+//
+//        Box box2 = new Box(box);
+//
+//        System.out.println(box2.l + " " + box2.h + " "+ box2.w);
+
+
+//        System.out.println(box2.l + " " + box2.h + " "+ box2.w);
+
+//        BoxWeight box3 = new BoxWeight();
+
+//        System.out.println(box3.l + " " + box3.h + " "+ box3.w + " " + box3.weight);
+
+//        BoxWeight box4 = new BoxWeight(2,3,4,5);
+//
+//        BoxWeight box5 = new BoxWeight(2,3,4,5);
+//
+//        System.out.println(box5.l + " " + box5.h + " "+ box5.w + " " + box5.weight);
+
+        BoxPrice box = new BoxPrice(5,8,200);
+
+
+    }
+}
