58c09b3f829b88545dbada2a1d7061aec08de542
ChetanSrikant <chetansmandiga@gmail.com>
2024-11-20 20:52:21 +0530

    doubley linked list

--- Linked_List/src/Main.java
+++ Linked_List/src/Main.java
@@ -7,23 +7,36 @@ public class Main {
 //        LinkedList<Integer> list= new LinkedList<>();   this is inbuilt form
 //        list.add(9);
 
-        LL list = new LL();
+//        LL list = new LL();
+//
+//        list.insertFirst(1);
+//        list.insertFirst(2);
+//        list.insertFirst(3);
+//        list.insertFirst(4);
+//        list.insert(5,2);
+
+//        list.display();
+//
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//
+//        System.out.println(list.deleteLast());
+//        list.display();
+//
+//        System.out.println(list.delete(1));
+//        list.display();
+
+
+        DLL list = new DLL();
 
         list.insertFirst(1);
         list.insertFirst(2);
         list.insertFirst(3);
         list.insertFirst(4);
-        list.insert(5,2);
+        list.insertLast(99);
+        list.insert(2,9);
 
         list.display();
-
-        System.out.println(list.deleteFirst());
-        list.display();
-
-        System.out.println(list.deleteLast());
-        list.display();
-
-        System.out.println(list.delete(1));
-        list.display();
+//        list.insert(5,2);
     }
 }
